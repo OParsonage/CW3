5f7942102c691972af75a443866f1ec36c93255c
Ben Joseph <64548001+BenJ101@users.noreply.github.com>
2023-03-22 16:54:53 +0000

    Working wavefront - valid only off previous iteration valid list

--- main.py
+++ main.py
@@ -52,9 +52,22 @@ grid7 = [
         [2, 0, 6, 0, 3, 0, 7, 0, 0]
 	    ]
 
+#9x9 grid of zeros
+grid8 = [
+		[0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0]
+		]
+
 #grids = [(grid1, 2, 2), (grid2, 2, 2), (grid3, 2, 2), (grid4, 2, 2), (grid5, 2, 2), (grid6, 2, 3)]
 #grids = [(grid1, 2, 2)]
-grids = [(grid7, 3, 3)]
+grids = [(grid8, 3, 3)]
 
 '''
 ===================================
@@ -147,14 +160,15 @@ def create_priority(grid, n_rows, n_cols, valid_array_old):
         priority_array: array of coordinates in the format [row_no, col_no, possible_values_count]
 	    valid_array: array of original grid with all 0-values replaced with viable values'''
 	priority_array = []
-	valid_array = [valid_array_old]
+	valid_array = valid_array_old
+	valid_array_old = to_tuple(valid_array_old)
 	n = n_rows*n_cols
 	for row in range(0, n): # i is the row
 		for column in range(0, n): # j is the column
 			if grid[row][column] == 0: # if the cell is empty
 				valid_array[row][column] = []
 				priority_array.append([row, column, []])
-				for value in valid_array_old[row][column]: # k is the number we are trying to put in the cell
+				for value in valid_array_old[row][column]: 
 					if valid(grid, row, column, value, n_rows, n_cols): # test that the value entered could be part of a valid solution
 						valid_array[row][column].append(value)
 						priority_array[-1][2].append(value)
